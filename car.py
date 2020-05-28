a7f3badd99c7e3667cce776961f78b679c7a2c9d
Mikolaj <mikolajb398@gmail.com>
2020-05-29 00:11:45 +0200

    Add curses view

--- car.py
+++ car.py
@@ -9,6 +9,7 @@ class Car(threading.Thread):
         threading.Thread.__init__(self)
         self.number = number
         self.lane = -1
+        self.state = ''
 
         self.avaiable_lines = avaiable_lines
         self.gas_stations = gas_stations
@@ -26,52 +27,55 @@ class Car(threading.Thread):
             if aviable and self.fuel>0 and self.car_durability>0 and self.tires_durability>0:
                 self.avaiable_lines[line].acquire()
                 self.line = line
+                self.state = f"Jedzie na torze: {line}"
                 self.drive()
                 self.avaiable_lines[line].release()
                 continue
             gas_station, aviable = self.check_avaiable_gas_stations()
             if aviable and self.fuel<=0:
                 self.gas_stations[gas_station].acquire()
+                self.state = f"Tankuje na stacji numer: {gas_station}"
                 self.fill_tank()
                 self.gas_stations[gas_station].release()
                 continue
             pitstop, aviable = self.check_avaiable_pitstops()
             if aviable and self.car_durability<=30:
                 self.pitstops[pitstop].acquire()
+                self.state = f"Jest w pitstopie numer: {pitstop}"
                 self.use_pitstop()
                 self.pitstops[pitstop].release()
                 continue
             tires_change_station, aviable = self.check_avaiable_tiers_change_stations()
             if aviable and self.tires_durability<=30:
                 self.tires_change_stations[tires_change_station].acquire()
+                self.state = f"Zmienia opony przy stacji: {tires_change_station}"
                 self.change_tires()
                 self.tires_change_stations[tires_change_station].release()
                 continue
-            print(f"car {self.number} is no able to drive. Distance {self.distance}. Fuel: {self.fuel}. Car durability {self.car_durability}. Tires durability: {self.tires_durability}.")
+            self.state = "Czeka"
             time.sleep(random.uniform(1, 5))
                 
     def drive(self):
-        self.distance += 1
-        self.fuel -= 50
-        self.car_durability -= 60
-        self.tires_durability -= 25
-        print(f"car {self.number} is driving on line {self.line}. Distance {self.distance}. Fuel: {self.fuel}. Car durability {self.car_durability}. Tires durability: {self.tires_durability}.")
-        time.sleep(random.uniform(1, 2))
+        self.distance += random.randint(1,3)
+        self.fuel -= random.randint(10,40)
+        if self.fuel < 0: self.fuel=0 
+        self.car_durability -= random.randint(10,60)
+        if self.car_durability < 0: self.car_durability=0 
+        self.tires_durability -= random.randint(5,20)
+        if self.tires_durability < 0: self.tires_durability=0 
+        time.sleep(random.uniform(1, 5))
 
     def fill_tank(self):
         self.fuel = 100
-        print(f"car {self.number} is getting gas. Distance {self.distance}. Fuel: {self.fuel}. Car durability {self.car_durability}. Tires durability: {self.tires_durability}.")
-        time.sleep(random.uniform(1, 2))
+        time.sleep(random.uniform(1, 5))
 
     def use_pitstop(self):
         self.car_durability = 100
-        print(f"car {self.number} is using pitstop. Distance {self.distance}. Fuel: {self.fuel}. Car durability {self.car_durability}. Tires durability: {self.tires_durability}.")
-        time.sleep(random.uniform(1, 2))
+        time.sleep(random.uniform(1, 5))
 
     def change_tires(self):
         self.tires_durability = 100
-        print(f"car {self.number} is changed tires. Distance {self.distance}. Fuel: {self.fuel}. Car durability {self.car_durability}. Tires durability: {self.tires_durability}.")
-        time.sleep(random.uniform(1, 2))
+        time.sleep(random.uniform(1, 5))
 
     def check_avaiable_lanes(self):
         for i in range(len(self.avaiable_lines)):
@@ -96,3 +100,14 @@ class Car(threading.Thread):
             if not self.tires_change_stations[i].locked():
                 return i, True
         return -1, False
+    
+    def __str__(self):
+        return f"Bolid {self.number}"
+
+    def get_car_info(self):
+        distance = f"Pokonany dystans: {self.distance}".ljust(21)
+        fuel = f"Stan paliwa: {self.fuel}".ljust(17)
+        car_durability = f"Stan samochodu: {self.car_durability}".ljust(20)
+        tires_durablity = f"Stan opon: {self.tires_durability}".ljust(20)
+
+        return f"{distance} {fuel} {car_durability} {tires_durablity}"
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from car import Car
 import threading
-
+import curses
 def fill_locks_array(array, count):
     for i in range(count):
         array.append(threading.Lock())
@@ -22,8 +22,32 @@ fill_locks_array(pitstops, pitstops_count)
 fill_locks_array(tires_change_stations, tires_change_stations_count)
 fill_locks_array(gas_stations, gas_stations_count)
 
-for i in range(car_count):
-    cars.append(Car(str(i), lines, gas_stations, pitstops, tires_change_stations))
+def main(stdscr):
+    stdscr.nodelay(1)
+    for i in range(car_count):
+        cars.append(Car(str(i), lines, gas_stations, pitstops, tires_change_stations))
+    
+    stdscr.clear()
 
-for car in cars:
-        car.start()
+    try:
+        stdscr.addstr(len(cars)+4, 0, "Nacisnij ESC aby wyjsc.")
+        stdscr.addstr(0, 0, "Tablica wynikow:")
+        
+        for car in cars:
+            car.start()
+        
+        while True:
+            sorted_cars = sorted(cars, key= lambda car: car.distance, reverse=True )  
+            stdscr.refresh()
+            key = stdscr.getch()
+            for i in range(len(sorted_cars)):
+                stdscr.addstr(i+2, 0, str(sorted_cars[i]))
+                stdscr.addstr(i+2, 10, sorted_cars[i].state.ljust(40))
+                stdscr.addstr(i+2, 40, sorted_cars[i].get_car_info())
+            if key == 27:
+                Car.driving = False
+                print("Zamykanie wszystkich watkow...")
+                break
+    except Exception as e:
+        print("Informacje o samochodach nie mieszcza sie w konsoli. Powieksz konsole lub zminejsz liczbe samochodow.")
+curses.wrapper(main)
